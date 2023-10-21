09d41e15dc28ee9a481cf10ac635db17186efac3
Patrick Bucher <patrick.bucher@sluz.ch>
2023-10-21 20:35:03 +0200

    example: multiplication table

--- SeparationOfConcerns.Demo/Program.cs
+++ SeparationOfConcerns.Demo/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using SeparationOfConcerns;
+
+public class Program
+{
+    public static void Main()
+    {
+        MultiplicationTable.For(new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 });
+    }
+}
--- SeparationOfConcerns/MultiplicationTable.cs
+++ SeparationOfConcerns/MultiplicationTable.cs
@@ -2,5 +2,54 @@
 
 public class MultiplicationTable
 {
+    public static void For(List<int> numbers)
+    {
+        // first, let's figure out the biggest value
+        var biggest = int.MinValue;
+        foreach (var number in numbers)
+        {
+            if (number < 0)
+            {
+                throw new ArgumentException("negative numbers are not supported");
+            }
+            if (number > biggest)
+            {
+                biggest = number;
+            }
+        }
 
+        // then, find the biggest possible result to compute its magnitude
+        var biggestResult = biggest * biggest;
+        var magnitude = 0;
+        while (biggestResult > 0)
+        {
+            magnitude++;
+            biggestResult /= 10;
+        }
+        magnitude++; // the magnitude (plus some space) will be used for the width
+
+        // finally, create the multiplication table, nicely formatted
+        var titleRow = "";
+        titleRow += "*".PadLeft(magnitude) + " ||";
+        foreach (var col in numbers)
+        {
+            titleRow += $"{col}".PadLeft(magnitude) + " |";
+        }
+        Console.WriteLine(titleRow);
+        for (var i = 0; i < titleRow.Length; i++)
+        {
+            Console.Write("=");
+        }
+        Console.WriteLine();
+        foreach (var row in numbers)
+        {
+            Console.Write($"{row}".PadLeft(magnitude) + " ||");
+            foreach (var col in numbers)
+            {
+                var product = row * col;
+                Console.Write($"{product}".PadLeft(magnitude) + " |");
+            }
+            Console.WriteLine();
+        }
+    }
 }
