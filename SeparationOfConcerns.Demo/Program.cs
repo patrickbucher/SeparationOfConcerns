196251f58d29b514cf42f16e545459bfaceebe48
Patrick Bucher <patrick.bucher@sluz.ch>
2023-10-22 09:34:37 +0200

    activate all examples in Main

--- SeparationOfConcerns.Demo/Program.cs
+++ SeparationOfConcerns.Demo/Program.cs
@@ -5,10 +5,10 @@ public class Program
     public static void Main()
     {
         // Example 1: Multiplication Table
-        // MultiplicationTable.For(new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 });
+        MultiplicationTable.For(new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 });
 
         // Example 2: Prime Factors
-        // PrimeFactors.Factor(new List<int>() {1, 2, 3, 4, 5, 6, 7, 8, 9, 10});
+        PrimeFactors.Factor(new List<int>() { 42, 99, 1234 });
 
         // Example 3: Monty Hall Problem
         MontyHall.Play(100);
